97c79bb4a26c4f492fd00ef63b387139d6107d82
Jesus Diaz <jesusdiaz24@tamu.edu>
2018-10-11 17:35:28 -0500

    Problem 1
    Code to go online and get the log file and then output how many requests were made in time period represented in the log.

--- log.py
+++ log.py
@@ -1,3 +1,16 @@
 import urllib.request
 import urllib.parse
+import re
 
+url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+response = str(urllib.request.urlopen(url).read())
+
+file = open("file.txt,", "w")
+file.write(response)
+
+
+#1
+total = ()
+total = re.findall(r' - - ', response)
+
+print (len(total))
